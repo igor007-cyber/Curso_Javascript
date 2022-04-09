45b64721c6308b75d2bb2e8b213ea4ed583c1c7f
igor ramalho <igor.ramalho007@gmail.com>
2022-04-09 14:26:57 -0300

    atualizado

---
+++ Aulas/aula9/aritmeticos.js
@@ -0,0 +1,39 @@
+/*
+    Aritmetica: 
+
+    + adicao / Concatenação
+    - subtração
+    / divisão
+    * multiplicação
+    ** Potenciação
+    % Resto da divisão
+    ++ incremento
+    -- decremento
+    parseInt -> vai passar para inteiro
+    parseFloat -> vai passar pra float  
+    Number -> vai passar para um numero
+*/
+const num1 = 5
+const num2 = 4
+const num3 = 10
+const nome = 'igor'
+const numero = '46'
+console.log(((num1 + num2**2)*num3)/2)
+let contador = 1
+++contador
+console.log(contador++)
+console.log("soma")
+contador += 2
+console.log(contador)
+console.log("mutiplicação")
+contador *= 2
+console.log(contador)
+console.log("divisao")
+contador /= 2
+console.log(contador)
+console.log("subtração")
+contador -= 2
+console.log(contador)
+
+console.log(parseInt(num1 + numero))
+
