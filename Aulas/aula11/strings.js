01b5ea239f83ca82103db4466fc4c3c510c16cc3
igor ramalho <igor.ramalho007@gmail.com>
2022-04-13 10:34:53 -0300

    atualizar

--- Aulas/aula11/strings.js
+++ Aulas/aula11/strings.js
@@ -6,3 +6,21 @@ console.log(umaString.charAt(5))// ele vai pegar alguma letra atravez de um veto
 console.log(umaString.concat(" num lindo dia")) //concatenar
 console.log(umaString.indexOf('um')) // vai dizer o indece da palvra ou letra;
 console.log(umaString.indexOf('t', 4)) //voce pode tambem achar o indice apartir de um determinado ponto
+console.log(umaString.lastIndexOf('m',3))
+console.log(umaString.match(/[a-z]/g)) //expressoes regulares
+console.log(umaString.search(/x/g)) // vai procurar onde esta o indece
+console.log(umaString.replace('um','Outro'))// vai substitui a palavra um para outa
+
+let outraString = 'O rato roeu a roupa do rei de roma'
+console.log(outraString.replace(/r/g, '#')) // vai pegar todos os r
+console.log(outraString.length)// vai ver o tamanho da string
+console.log(outraString.slice(2,5))
+console.log(outraString.slice(-3))
+console.log(outraString.slice(-5,-1))
+console.log(outraString.substring(outraString.length - 5, outraString.length - 1))
+console.log(outraString.split(' ', 2))
+console.log(outraString.toUpperCase()) //tudo maiuculo
+console.log(outraString.toLowerCase()) //tudo minusculo
+
+
+
