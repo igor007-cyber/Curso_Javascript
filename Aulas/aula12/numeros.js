2e4701f28be27369af3e0e2dc322110d1aed7987
igor ramalho <igor.ramalho007@gmail.com>
2022-04-22 11:37:49 -0300

    aulas

---
+++ Aulas/aula12/numeros.js
@@ -0,0 +1,23 @@
+// Mais sobre numeros
+
+let num = 10
+let num1 = 2.5
+let num2 = 10.45215646486
+let num3 = 0.7
+let num4 = 0.1
+// num1 += num2 -> da a mesma coisa somando eles dois
+num3 = (((num3 * 100) + (num4 * 100))/100)
+num3 = (((num3 * 100) + (num4 * 100))/100)
+num3 = (((num3 * 100) + (num4 * 100))/100)
+console.log(num3)
+console.log(Number.isInteger(num3))
+//console.log(num3 + num4)
+// console.log(num.toString() + num1)
+// num1 = num1.toString()
+console.log(typeof num1)
+console.log(num.toString(2))// representação binaria
+console.log(num2.toFixed(2))
+console.log(Number.isInteger(num1))
+let temp = num * 'ola'
+console.log(temp) 
+console.log(Number.isNaN(temp))
---
+++ Aulas/aula13/objetoMath.js
@@ -0,0 +1,20 @@
+let num = 9.431234
+let num1 = Math.floor(num) // Aqui está arredondando pra baixo
+let num2 = Math.ceil(num) // arredonda pra cima
+let num3 = Math.round(num) // se for acima de 5 ele arredonda pra cima, se for abaixo ele arredonda pra baixo
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+let num4 = 9
+
+console.log(aleatorio)
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(Math.max(1,2,3,4,5,6,-10,-50,1500,9,8,7,6)) // vai pegar o numero maximo
+console.log(Math.min(1,2,3,4,5,6,-10,-50,1500,9,8,7,6)) // vai pegar o numero minimo
+console.log(Math.random()) // vai colocar um numero aleatorio
+console.log(Math.PI) // pi
+console.log(Math.pow(2,10)) // um numero elevado a outro
+console.log(2**5)// um numero elevado a outro
+console.log(Math.sqrt(num4)) // raiz quadrada
+console.log(num4 ** (1/2)) // tirar a raiz quadrada
+console.log(100/0) //observe o resultado    
